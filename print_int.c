e4146f0420850cc917bd25db5696662700659f9a
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-10 23:41:03 +0000

    merge issues

--- print_int.c
+++ print_int.c
@@ -41,34 +41,4 @@ int print_int(int n)
     }
     collab_putchar(last_digit + '0');
     return (count); 
-}/*
-	if (last_digit < 0)
-	{
-		collab_putchar('-');
-		num = -num;
-		n = -n;
-		last_digit = -last_digit;
-		count++;
-	}
-	if (num > 0)
-	{
-	while (num / 10 != 0)
-	{
-		power_of_10 = power_of_10 * 10;
-		num = num / 10;
-	}
-	num = n;
-	while (power_of_10 > 0)
-	{
-		digit = num / power_of_10;
-		collab_putchar(digit + '0');
-		num = num - (digit * power_of_10);
-		power_of_10 = power_of_10 / 10;
-		count++;
-	}
-	}
-	collab_putchar(last_digit + '0');
-	return (count);
-
 }
-    */
