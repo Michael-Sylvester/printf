ead0393a3e93a5c3bd12221785d31686fcdea4fb
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-10 12:50:11 +0000

    _printf edit

--- _printf.c
+++ _printf.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
-#include "print.h"
+#include "main.h"
 /* *
- * myprintf - a function to replicate the workings of printf
+ * _printf - a function to replicate the workings of printf
  * @format : the string entered by the user
  */
 
-void myprintf(const char *format, ...)
+int _printf(const char *format, ...)
 {
   /* va_list name, 
    * va_start(list_name, format), 
    * va_arg(list_name, next_format_specifier_data_type)
  */
+  int i;
+  char c;
+  char * str;
   va_list ap;
+  
   va_start(ap, format);
   while(*format != '\0')
     {
@@ -23,21 +27,33 @@ void myprintf(const char *format, ...)
 	  switch (*format)
 	    {
 	    case 'd':
-	      /* print intreger function */
+	      /* print interger function */
+	      i = va_arg(ap, int);
+	      print_int(i);	
 	      break;
 	    case 'i':
 	      /* print integer function */
+	      i = va_arg(ap, int);
+	      print_int(i);
 	      break;
 	    case 'c':
 	      /* print character function */
+	      c = (char)va_arg(ap, int);
+	      _putchar(i);
 	      break;
 	    case 's':
 	      /* print string function */
+	      str = va_arg(ap, char *);
+	      print_string(str);
 	      break;
-	    }
-	}     /* if keyword not available print current member of format  */
+	    default:
+	      break;
+	    } /* end of switch statement */
+	}     /* end of if statement */
+      /* if keyword not available print current member of format string */
       _putchar(*format);
       format++;  
-    }
+    } /* end of while loop */
   va_end(ap);
+  return (0);
 }
