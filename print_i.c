8bb3bc0dab6248d441626f815c7fcfd897117723
Likeness30 <likeness14@gmail.com>
2023-11-10 14:16:30 -0800

    print_i.c

---
+++ print_i.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_i - This function prints an integer
+ * @args: Arguments to print
+ * Return: An integer
+ */
+int print_i(va_list args)
+{
+    int n = va_arg(args, int);
+    int num, last_digit = n % 10, digit, power_of_10 = 1;
+    int count = 1;  /* initialized count to 1 */
+
+    n = n / 10;
+    num = n;
+
+    if (last < 0)
+    {
+        collab_putchar('-');
+        num = -num;
+        n = -n;
+        last_digit = -last_digit;
+        count++;
+    }
+    if (num > 0)
+    {
+        while (num / 10 != 0)
+        {
+            power_of_10 = power_of_10 * 10;
+            num = num / 10;
+        }
+        num = n;
+        while (power_of_10 > 0)
+        {
+            digit = num / power_of_10;
+            collab_putchar(digit + '0');
+            num = num - (digit * power_of_10);
+            power_of_10 = power_of_10 / 10;
+            count++;
+        }
+    }
+    collab_putchar(last_digit + '0');
+    return (count);
+}
