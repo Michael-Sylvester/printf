696a6a22c54dd4f06b113efadda62041a777071b
Likeness30 <likeness14@gmail.com>
2023-11-10 07:11:31 -0800

    Updated main.h

--- main.h
+++ main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int collab_putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
