b7e7a61dc5b253885b9121c364eff4cb328dd026
Michael-Sylvester <mike.k.sylvester@gmail.com>
2023-11-13 15:26:48 +0000

    betty_all script

--- print_pointer.c
+++ print_pointer.c
@@ -8,27 +8,27 @@
 
 int print_pointer(void *ptr)
 {
-  unsigned long address = (unsigned long)ptr; /* Cast the pointer to an unsigned long */
-  int count = 0;
-  int i;
-  int hex_digit;
-  /* Print the address in hexadecimal format */
-  collab_putchar('0');
-  collab_putchar('x');
+	/* Cast the pointer to an unsigned long */
+	unsigned long address = (unsigned long)ptr;
+	int count = 0;
+	int i;
+	int hex_digit;
+	/* Print the address in hexadecimal format */
+	collab_putchar('0');
+	collab_putchar('x');
 
-  /* Print the hexadecimal digits of the address */
-  for (i = (sizeof(void*) * 2 - 1) * 4; i >= 0; i -= 4) {
-    hex_digit = (address >> i) & 0xF;
+	/* Print the hexadecimal digits of the address */
+	for (i = (sizeof(void *) * 2 - 1) * 4; i >= 0; i -= 4)
+		{
+			hex_digit = (address >> i) & 0xF;
 
-    if (hex_digit < 10)
-      collab_putchar(hex_digit + '0');
-    else
-      collab_putchar(hex_digit + 'a' - 10);
+			if (hex_digit < 10)
+				collab_putchar(hex_digit + '0');
+			else
+				collab_putchar(hex_digit + 'a' - 10);
 
-    count++;
-  }
+			count++;
+		}
 
-  return count;
-
-
+	return (count);
 }
