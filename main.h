b5337b58e09c0ac7da7af6f944bcfb50ffd0419b
Likeness30 <likeness14@gmail.com>
2023-11-13 07:42:00 -0800

    Updated main.h

--- main.h
+++ main.h
@@ -27,6 +27,9 @@ int print_int(int n);
 int print_b(int num);
 int print_pointer(void *);
 int print_octal(unsigned int);
+int convert_size_unsgnd(unsigned long int num, int size);
+int write_unsgnd(int arg1, int arg2, char buffer[],
+int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
